4222acd0341768b4e2ec42b39985db90e2cf942a
Mori <jardenlee@163.com>
2023-03-12 15:04:20 +0800

    Initial Commit

---
+++ Gimpass 1.0/Gimpass_1_0App.swift
@@ -0,0 +1,17 @@
+//
+//  Gimpass_1_0App.swift
+//  Gimpass 1.0
+//
+//  Created by MORI on 2023/3/12.
+//
+
+import SwiftUI
+
+@main
+struct Gimpass_1_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
